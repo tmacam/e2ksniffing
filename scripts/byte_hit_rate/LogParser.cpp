b01ef22e6ac6c3e89c0376abd1899b5353f343f0
Tiago Alves Macambira <tmacam@burocrata.org>
2004-05-06 21:18:54 +0000

    Evitando um erro...
    git-svn-id: file:///scratch/tmacam/mestrado-svn/trunk@93 7080319d-1156-4bb6-a8bf-3a6c6baf4512

--- scripts/byte_hit_rate/LogParser.cpp
+++ scripts/byte_hit_rate/LogParser.cpp
@@ -1,7 +1,7 @@
 /**@file LogParser.cpp
  * @brief LogParser - implementation
  * @author Tiago Alves Macambira <tmacam () dcc ufmg br>
- * @version $Id: LogParser.cpp,v 1.1 2004-05-06 06:54:00 tmacam Exp $
+ * @version $Id: LogParser.cpp,v 1.2 2004-05-06 21:18:54 tmacam Exp $
  */
 
 #include <sstream>
@@ -31,7 +31,7 @@ void LogParser::get_offset(istream& iss, offset_t& of)
 {
 	iss.ignore(10,'[');
 	iss >> of.first;
-	iss.ignore();
+	iss.ignore(1,',');
 	iss >> of.second;
 }
 
@@ -49,14 +49,18 @@ void LogParser::process_line(string& line)
 		// Get the fields hash[] offset[]
 		get_hash(line_stream,hash);
 		get_offset(line_stream,offset);
-		onSendingPart(hash,offset);
+		if ( line_stream.good() ){
+			onSendingPart(hash,offset);
+		}
 	} else if ( (pos = line.find(str_compressed_part)) != string::npos) {
 		pos += str_compressed_part_len;	 //"Compressed emule data "
 		line_stream.str(line.substr(pos));
 		// Get the fields hash[] offset[]
 		get_hash(line_stream,hash);
 		get_offset(line_stream,offset);
-		onSendingCompressed(hash,offset);
+		if ( line_stream.good() ){
+			onSendingCompressed(hash,offset);
+		}
 	}
 
 }
